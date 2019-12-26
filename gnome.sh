ed3157e585474757ffce918a5756b71957aeedfc
daisukeshun <daisukeshun1@gmail.com>
2019-12-26 18:36:27 +0300

    gnome workspaces and dock shortcuts

--- gnome.sh
+++ gnome.sh
@@ -1,3 +1,6 @@
 sudo apt remove gnome-shell-extension-ubuntu-dock -y
-gsettings set org.gnome.desktop.wm.preferences button-layout 'close,maximize:'
 sudo apt autoremove -y
+gsettings set org.gnome.desktop.wm.preferences button-layout 'close,maximize:'
+gsettings set org.gnome.shell.extensions.dash-to-dock hot-keys false
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 4
