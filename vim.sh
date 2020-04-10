b055c7c8af6bda6ea51a1ecbb7fba2ebe576a312
daisukeshun <daisukeshun1@gmail.com>
2020-04-10 14:15:18 +0300

    add fugitive, remove nerdtree

--- vim.sh
+++ vim.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+echo "this programm must run with common user (NOT ROOT!)"
 sudo apt install git -y
 sudo apt install vim -y
 sudo apt install cmake -y
 sudo apt install python3-dev -y
 
 cd ~/
-git clone https://github.com/daisukeshun/.vimrc-and-.vim.git
+git clone https://github.com/daisukeshun/dotfiles.git
 
-cp -r .vimrc-and-.vim/* ./
+cp -r dotfiles/* ./
 mv ./vim ./.vim
-rm -rf .vimrc-and-.vim LICENSE README.md
+rm -rf dotfiles LICENSE README.md
 
 cd .vim/bundle
+git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/ycm-core/YouCompleteMe.git
-git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/beyondmarc/opengl.vim
 git clone https://github.com/zeis/vim-kolor.git
 
 cd ./YouCompleteMe
 git submodule update --init --recursive
 python3 install.py --clang-completer
+
 
 echo "All works fine"
