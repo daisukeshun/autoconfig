d6d18b517663eb4478cb44233131f2cc3aa65f82
daisukeshun <daisukeshun1@gmail.com>
2020-05-23 13:33:28 +0300

    updated vim.sh

--- vim.sh
+++ vim.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "this programm must run with common user (NOT ROOT!)"
+
 sudo apt install git -y
 sudo apt install vim -y
 sudo apt install cmake -y
 sudo apt install python3-dev -y
+sudo apt install build-essential -y
 
 cd ~/
 git clone https://github.com/daisukeshun/dotfiles.git
@@ -16,11 +18,9 @@ cd .vim/bundle
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/ycm-core/YouCompleteMe.git
 git clone https://github.com/beyondmarc/opengl.vim
-git clone https://github.com/zeis/vim-kolor.git
 
-cd ./YouCompleteMe
+cd ~/.vim/bundle/YouCompleteMe
 git submodule update --init --recursive
-python3 install.py --clang-completer
-
+python3 install.py --clangd-completer
 
 echo "All works fine"
