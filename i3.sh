cb9e405942c475da34ea666bb1f89691b546264b
daisukeshun <daisukeshun1@gmail.com>
2020-08-21 18:29:25 +0300

    i3wm config added

--- i3.sh
+++ i3.sh
@@ -3,7 +3,7 @@ echo "this programm must run with common user (NOT ROOT!)"
 
 cd ~/
 git clone https://github.com/daisukeshun/i3wm-config.git
-cp ~/i3wm-config/* ~/.config/
+cp ~/i3wm-config/* ~/.config/i3/
 
 
 
